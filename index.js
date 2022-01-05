35e4b5c00cc08a1eeecb875df2024ec543866ec5
Elvan H <elvanhd@gmail.com>
2022-01-05 18:46:37 +0700

    mengirim data dari express dan menambahkan middleware untuk cors

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 const express = require('express');
 
+const enableCors = require('./middleware/cors.js');
 const products = require('./seeds/products');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(enableCors);
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Express Backend is working');
 });
 
 app.get('/api/products', (req, res) => {
-  res.json({
-    data: {
-      products: products,
-    },
-    meta: {
-      status: 200,
-      message: 'Success',
-    },
-  });
+  res.json({ products: products });
 });
 
 app.get('/api/products/:id', (req, res) => {
   const { id } = req.params;
   const product = products.find((product) => product._id === id);
 
-  res.json({
-    data: {
-      product: product,
-    },
-    meta: {
-      status: 200,
-      message: 'Success',
-    },
-  });
+  res.json({ product: product });
 });
 
 app.listen(PORT, () => {
